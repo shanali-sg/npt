854e305ec0178e627121cb4630175e9776e5ce4d
barriejc <73184215+barriejc@users.noreply.github.com>
2022-10-15 16:44:47 -0700

    Added some print loops to learn spf and simple paths in nx

--- npt.py
+++ npt.py
@@ -2,4 +2,12 @@ import networkx as nx
 G = nx.Graph()
 G = nx.read_edgelist("npt.edgelist", nodetype=int)
 G = nx.Graph(((u, v, e) for u,v,e in G.edges(data=True) if e['capacity'] == 1))
-print(nx.shortest_path(G, 1, 5))
+
+print("shortest paths")
+for (u,v,e) in G.edges(data=True):
+    print(nx.shortest_path(G, u, v))
+
+for (u,v,e) in G.edges(data=True):
+    print("simple paths %s to %s" % (u, v))
+    for path in nx.all_simple_paths(G, source=u, target=v):
+        print(path)
