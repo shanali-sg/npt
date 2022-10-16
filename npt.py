7a6549e4230b6ee9ef015f0c29422e42d6b74481
shanali <shanali@yahoo.com>
2022-10-16 16:11:00 -0700

    turned pseudo code to code

--- npt.py
+++ npt.py
@@ -19,12 +19,28 @@ candidate_route_ref = []
 diverse_route_ref = []
 
 # loop through all available paths
-for p in paths:
+for p in wps:
     #print(p, nx.path_weight(G, p, weight='distance') )
     
-    # for p determine the ref. segments
-    
-         
+    # for p determine the ref. segments for all the edges
+    edge_index = 0
+    for l in len(p):
+        attr = G[p[edge_index]][p[edge_index+1]]
+        edge_index =+ 1
+        edge_ref = attr["ref"]
+        if candidate_route_ref == []:
+            candidate_route_ref = candidate_route_ref.insert(edge_ref)
+        else:
+            candidate_route_ref = candidate_route_ref.remove(edge_ref)
+            if diverse_route_ref == []:
+                diverse_route_ref = diverse_route_ref.insert(edge_ref)
+            else:
+                diverse_route_ref = diverse_route_ref.remove(edge_ref)
+        
+        if candidate_route_ref != diverse_route_ref:
+            print(candidate_route)
+            print(diverse_route)
+
     # if candidate_route_ref has no existing ref.
     # record the ref
     # if diverse_route_ref
